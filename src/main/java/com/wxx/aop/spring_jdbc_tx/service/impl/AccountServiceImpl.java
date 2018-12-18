22a119a7c54ae53b3d834101956eedae68c312d1
wuxinxin1 <2468732919@qq.com>
2018-12-18 18:39:43 +0800

    z

--- src/main/java/com/wxx/aop/spring_jdbc_tx/service/impl/AccountServiceImpl.java
+++ src/main/java/com/wxx/aop/spring_jdbc_tx/service/impl/AccountServiceImpl.java
@@ -2,11 +2,21 @@ package com.wxx.aop.spring_jdbc_tx.service.impl;
 
 import com.wxx.aop.spring_jdbc_tx.dao.AccountDao;
 import com.wxx.aop.spring_jdbc_tx.service.AccountService;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 /**
  * Created by wuxinxin on 2018/12/18.
  */
 public class AccountServiceImpl implements AccountService {
+
+
+    /**
+     * 使用transactionTemplate操作事务
+     */
+    private TransactionTemplate transactionTemplate;
 
     private AccountDao accountDao;
 
@@ -14,11 +24,22 @@ public class AccountServiceImpl implements AccountService {
         this.accountDao = accountDao;
     }
 
+    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+        this.transactionTemplate = transactionTemplate;
+    }
+
     @Override
-    public void transfer(String outter,String inner, int money) {
-        accountDao.out(outter,money);
-        //断电
-        int a=10/0;
-        accountDao.into(inner,money);
+    public void transfer(final String outter, final String inner, final int money) {
+        transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus status) {
+                accountDao.out(outter,money);
+                //断电
+                int a=10/0;
+                accountDao.into(inner,money);
+                return null;
+            }
+        });
+
     }
 }
